cdd2688e1d212465efe932352a42b14f752c4bf9
cleberMargarida <cleberfilho3@gmail.com>
2023-06-01 16:14:38 -0300

    Add project files.

---
+++ RedisBenchmark/Program.cs
@@ -0,0 +1,101 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using Ingestion.Api.Dto;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<RedisBenchmark>();
+    }
+}
+
+[SimpleJob(RuntimeMoniker.Net60, baseline: true)]
+public class RedisBenchmark
+{
+    private SignalDataDto[] signals;
+    private ConnectionMultiplexer redis;
+    private IDatabase db;
+    private ISubscriber pubsub;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        // Initialize the ConnectionMultiplexer once before running the benchmarks
+        redis = ConnectionMultiplexer.Connect("");
+        db = redis.GetDatabase();
+        pubsub = redis.GetSubscriber();
+        signals = new[]
+        {
+            new SignalDataDto("Device1", "Object1", 1621123456, "Value1"),
+            new SignalDataDto("Device2", "Object2", 1621123457, "Value2"),
+            new SignalDataDto("Device3", "Object3", 1621123458, "Value3"),
+            new SignalDataDto("Device4", "Object4", 1621123459, "Value4"),
+            new SignalDataDto("Device5", "Object5", 1621123460, "Value5"),
+            new SignalDataDto("Device6", "Object6", 1621123461, "Value6"),
+            new SignalDataDto("Device7", "Object7", 1621123462, "Value7"),
+            new SignalDataDto("Device8", "Object8", 1621123463, "Value8"),
+            new SignalDataDto("Device9", "Object9", 1621123464, "Value9"),
+            new SignalDataDto("Device10", "Object10", 1621123465, "Value10")
+        };
+        _ = signals.Select(x => x.HashCode);
+
+    }
+
+    /*
+    |                                 Method |      Mean |    Error |   StdDev | Ratio |
+    |--------------------------------------- |----------:|---------:|---------:|------:|
+    |         InsertSignalsOnDatabaseBatched |  21.41 ms | 0.231 ms | 0.205 ms |  1.00 |
+    |                                        |           |          |          |       |
+    | InsertSignalsOnDatabaseNotBatchedAsync | 211.83 ms | 2.597 ms | 2.429 ms |  1.00 |
+    |                                        |           |          |          |       |
+    |                    SendSignalsOnPubSub |  20.95 ms | 0.248 ms | 0.232 ms |  1.00 | 
+     */
+
+    [Benchmark]
+    public void InsertSignalsOnDatabaseBatched()
+    {
+        var batch = db.CreateBatch();
+        var tasks = GetInsertions().ToArray();
+        batch.Execute();
+
+        Task.WaitAll(tasks);
+
+        IEnumerable<Task<bool>> GetInsertions()
+        {
+            foreach (var signal in signals)
+                yield return batch.SetAddAsync(signal.HashCode.ToString(), JsonConvert.SerializeObject(signal));
+        }
+    }
+
+    [Benchmark]
+    public async Task InsertSignalsOnDatabaseNotBatchedAsync()
+    {
+        foreach (var signal in signals)
+            await db.SetAddAsync(signal.HashCode.ToString(), JsonConvert.SerializeObject(signal));
+    }
+
+    [Benchmark]
+    public void SendSignalsOnPubSub()
+    {
+        pubsub.WaitAll(GetPublications().ToArray());
+
+        IEnumerable<Task> GetPublications()
+        {
+            foreach (var signal in signals)
+                yield return pubsub.PublishAsync("benchmarkChannel", JsonConvert.SerializeObject(signal));
+        }
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        redis.Dispose();
+    }
+}
---
+++ RedisBenchmark/SignalDataDto.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ingestion.Api.Dto
+{
+    /// <summary>
+    /// Represents the data of a signal received from a device. Here is a breakdown of the class:
+    /// </summary>
+    public class SignalDataDto
+    {
+        private Guid? _hashCode;
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the device sending the signal.
+        /// </summary>
+        [JsonProperty("deviceId")]
+        public string DeviceId { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the object associated with the signal.
+        /// </summary>
+        [JsonProperty("id")]
+        public string ObjectId { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the Unix timestamp (in milliseconds) indicating when the signal was generated.
+        /// </summary>
+        [JsonProperty("timestamp")]
+        public long TimeStamp { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the value associated with the signal.
+        /// </summary>
+        [JsonProperty("value")]
+        public string? Value { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets an array of key-value pairs associated with the gps signal.
+        /// </summary>
+        [JsonProperty("keyValue")]
+        public KeyValuePair<long, double>[] KeyValue { get; set; } = default!;
+
+        /// <summary>
+        /// Gets the UTC date and time when the signal was generated.
+        /// </summary>
+        [JsonIgnore]
+        public DateTime DateTimeUTC => DateTime.UnixEpoch.AddMilliseconds(TimeStamp);
+
+        /// <summary>
+        /// Gets or sets the vehicle number associated with the signal.
+        /// </summary>
+        [JsonIgnore]
+        public string VehicleNumber { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the speed associated with the signal.
+        /// </summary>
+        [JsonIgnore]
+        public float? Speed { get; set; } = default!;
+
+        /// <summary>
+        /// Gets or sets the unit text associated with the signal.
+        /// </summary>
+        [JsonIgnore]
+        public string? UnitText { get; set; } = default!;
+
+        /// <summary>
+        /// Gets the hash code for the signal data.
+        /// </summary>
+        [JsonIgnore]
+        public Guid HashCode => _hashCode ??= Guid.Parse(GetMd5HashCode());
+
+        /// <summary>
+        /// Gets or sets the tenant identifier for the signal data.
+        /// </summary>
+        [JsonIgnore]
+        public int TenantId { get; set; } = default!;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignalDataDto"/> class.
+        /// </summary>
+        public SignalDataDto()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignalDataDto"/> class with the specified device ID, object ID, timestamp, and value.
+        /// </summary>
+        /// <param name="deviceId">The unique identifier of the device sending the signal.</param>
+        /// <param name="objectId">The unique identifier of the object associated with the signal.</param>
+        /// <param name="timeStamp">The Unix timestamp (in milliseconds) indicating when the signal was generated.</param>
+        /// <param name="value">The value associated with the signal.</param>
+        public SignalDataDto(string deviceId, string objectId, long timeStamp, string? value)
+        {
+            DeviceId = deviceId;
+            ObjectId = objectId;
+            TimeStamp = timeStamp;
+            Value = value;
+        }
+
+        private string GetMd5HashCode()
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            var input = ObjectId + TimeStamp + DeviceId + (Value?.ToString() ?? JsonConvert.SerializeObject(KeyValue) ?? "");
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
+
